21e0338820b430017f5e7b7ea03389a98f2cac33
oimo23 <oimo2338@gmail.com>
2019-10-03 02:14:13 +0900

    ConstantsSample.swiftを追加

---
+++ oTenki/Model/ConstantsSample.swift
@@ -0,0 +1,25 @@
+//
+//  Constants.swift
+//  oTenki
+//
+//  Created by 伏貫祐樹 on 2019/09/22.
+//  Copyright © 2019 yuki fushinuki. All rights reserved.
+//
+
+import Foundation
+
+// 定数クラスのサンプル
+struct ConstantsSample {
+    
+    // APIのURLとAPP_ID
+    let WEATHER_URL: String
+    let APP_ID: String
+    
+    init() {
+        
+        self.WEATHER_URL = "http://api.openweathermap.org/data/2.5/weather"
+        self.APP_ID = "YOUR_APP_ID"
+        
+    }
+    
+}
